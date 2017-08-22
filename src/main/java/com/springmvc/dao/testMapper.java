32cecb17375f45df3d9075e9fa5658f97dccd011
liushangyu <liushangyu@baidu.com>
2017-08-22 10:44:03 +0800

    增添了mybatis的xml文件的使用

--- src/main/java/com/springmvc/dao/testMapper.java
+++
@@ -1,9 +0,0 @@
-package com.springmvc.dao;
-import org.apache.ibatis.annotations.Select;
-/**
- * Created by apple on 2017/7/27.
- */
-public interface testMapper {
-    //@Select("SELECT number FROM hoho WHERE id = #{id}")
-    public int testSql(int id);
-}
--- src/main/java/com/springmvc/service/TestService.java
+++
@@ -1,8 +0,0 @@
-package com.springmvc.service;
-
-/**
- * Created by apple on 2017/7/27.
- */
-public interface TestService {
-    int testSqlService(int id);
-}
